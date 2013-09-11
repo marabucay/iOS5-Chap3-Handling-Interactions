54ccb7ffcf2bf32627d9f687d98fd8a164f8ad49
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-11 15:27:02 +0800

    Added left and right button and their implementation..

--- ButtonFun/ButtonFun/BIDViewController.h
+++ ButtonFun/ButtonFun/BIDViewController.h
@@ -1,13 +1,7 @@
-//
-//  BIDViewController.h
-//  ButtonFun
-//
-//  Created by Maria Alice C. Lim on 9/11/13.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface BIDViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UILabel *statusText;
+- (IBAction)buttonPressed:(id)sender;
+    
 @end
--- ButtonFun/ButtonFun/BIDViewController.m
+++ ButtonFun/ButtonFun/BIDViewController.m
@@ -1,60 +1,22 @@
 //
 //  BIDViewController.m
 //  ButtonFun
-//
-//  Created by Maria Alice C. Lim on 9/11/13.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
-//
+
 
 #import "BIDViewController.h"
 
 @implementation BIDViewController
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
-}
-
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+@synthesize statusText;
 
 - (void)viewDidUnload
 {
+    [self setStatusText:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
+- (IBAction)buttonPressed:(UIButton *)sender {
+    NSString *title = [sender titleForState:UIControlStateNormal];
+    statusText.text = [NSString stringWithFormat: @"%@ button pressed.", title];
 }
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
-
 @end
